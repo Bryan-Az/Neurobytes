3522bffb6e9681e796298499042721c71e007ea7
Alexis-Az <guoxuyang@Xuyangs-MacBook-Pro.local>
2024-05-07 12:24:40 -0700

    datadir set to db in mlops/scripts/ config

--- mlops/scripts/create_user_preferences/src/config.py
+++ mlops/scripts/create_user_preferences/src/config.py
@@ -2,4 +2,4 @@ import os
 
 # Configuration for data paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, 'data')
+DATA_DIR = os.path.dirname(os.path.abspath('../../../db/data/README.md'))
